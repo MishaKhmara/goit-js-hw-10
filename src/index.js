becce8f748b7f9284d7de4ca68d8584d9ddf306d
MishaKhmara <72788711+MishaKhmara@users.noreply.github.com>
2021-03-06 10:27:46 +0200

    исправил ошибки

--- src/index.js
+++ src/index.js
@@ -33,35 +33,33 @@ const Theme = {
     toggleRef: document.querySelector("#theme-switch-toggle"),
     body:document.querySelector('body')
   }
-  refs.body.classList.add(Theme.LIGHT)
+  
 
   refs.toggleRef.addEventListener('change',onToggleChange)
-  refs.toggleRef.addEventListener('change',setLocalStorage)
+  
  
 
 
 
   function onToggleChange (){
    refs.body.classList.toggle(Theme.DARK)
-  
+   
+   const check = refs.toggleRef.checked;
+
+   if (check) {
+     localStorage.setItem('theme', Theme.DARK);
+   } else {
+     localStorage.setItem('theme', Theme.LIGHT);
+   }
+
+
+
   }
 //------------------------------- localStorage ------------------------------//
-
-
-function setLocalStorage(e) {
-  const check = refs.toggleRef.checked;
-
-  if (check) {
-    localStorage.setItem('theme', Theme.DARK);
-  } else {
-    localStorage.removeItem('theme');
-    localStorage.setItem('theme', Theme.LIGHT);
-  }
-}
 
 const themeInLocal = localStorage.getItem('theme');
 
 if (themeInLocal === Theme.DARK) {
   refs.body.classList.add(Theme.DARK);
   refs.toggleRef.checked = true;
-}
+}else{refs.body.classList.toggle(Theme.LIGHT)};
